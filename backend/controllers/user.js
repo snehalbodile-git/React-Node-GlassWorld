093796608d0c4edea36ec6bc54d944dedd3ef96d
Snehal Bodile <contact.snehalbodile@gmail.com>
2026-04-15 13:39:01 +0530

    Solved testing issue of CRUD API and send response correclty

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,27 +1,29 @@
-const { json } = require('body-parser');
-const User = require('../models/user');
+const { json } = require("body-parser");
+const User = require("../models/user");
 
-async function handleGetAllUsers(req,res){
-    const allUsers = await User.find({});
-    return res.json(allUsers);
+async function handleGetAllUsers(req, res) {
+  const allUsers = await User.find({});
+  return res.json({
+    success: true,
+    data: allUsers,
+  });
 }
 
-async function handleGetUserById(req,res){
-    const user = await User.findById(req.params.id);
-    if(!user) return res.status(404).json({erro:'User not found'});
-    return res.json(user);
+async function handleGetUserById(req, res) {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).json({ erro: "User not found" });
+  return res.json({
+    success: true,
+    data: user,
+  });
 }
 
-async function handleUpdateUserById(req,res){
+async function handleUpdateUserById(req, res) {
   try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,          // return updated document
-        runValidators: true // validate schema rules
-      }
-    );
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // return updated document
+      runValidators: true, // validate schema rules
+    });
 
     if (!updatedUser) {
       return res.status(404).json({ msg: "User not found" });
@@ -29,51 +31,56 @@ async function handleUpdateUserById(req,res){
 
     return res.status(200).json({
       msg: "User updated successfully",
-      data: updatedUser
+      data: updatedUser,
     });
-
   } catch (error) {
     return res.status(500).json({
       msg: "Server Error",
-      error: error.message
+      error: error.message,
     });
   }
 }
 
-async function handleDeleteUserById(req,res){
-    await User.findByIdAndDelete(req.params.id);
-    return res.json({status: 'Success'});
+async function handleDeleteUserById(req, res) {
+  await User.findByIdAndDelete(req.params.id);
+  return res.json({ status: "Success" });
 }
 
-async function handleCreateNewUser(req,res){
-    const body = req.body;
-    if(
-        !body ||
-        !body.firstName ||
-        !body.email ||
-        !body.phone ||
-        !body.role ||
-        !body.status
-    ) return res.status(400).json({'msg': 'All feild required'})
+async function handleCreateNewUser(req, res) {
+  const body = req.body;
+  if (
+    !body ||
+    !body.firstName ||
+    !body.email ||
+    !body.phone ||
+    !body.role ||
+    !body.status
+  )
+    return res.status(400).json({ msg: "All feild required" });
 
-    const result = await User.create({
-        firstName: body.firstName,
-        lastName: body.lastName,
-        email: body.email,
-        phone: body.phone,
-        address: body.address,
-        role: body.role,
-        status: body.status,
+  const existingUser = await User.findOne({ email: body.email });
+  if (existingUser) {
+    return res.status(400).json({
+      msg: "Email already exists",
     });
+  }
+  const result = await User.create({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    email: body.email,
+    phone: body.phone,
+    address: body.address,
+    role: body.role,
+    status: body.status,
+  });
 
-    return res.status(201).json({msg:"Success",id:result._id});
+  return res.status(201).json({ msg: "Success", id: result._id });
 }
 
 module.exports = {
-    handleGetAllUsers,
-    handleGetUserById,
-    handleUpdateUserById,
-    handleDeleteUserById,
-    handleCreateNewUser
-}
-
+  handleGetAllUsers,
+  handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateNewUser,
+};
