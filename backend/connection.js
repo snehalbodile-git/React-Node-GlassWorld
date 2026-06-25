5e68090a33a889ae0cc6557ebc4de0159e29fa09
Snehal Bodile <contact.snehalbodile@gmail.com>
2026-06-25 12:28:55 +0530

    backend/connect atlast mongoDB

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
 
-async function connectMongoDb(){
-    return mongoose.connect('mongodb://127.0.0.1:27017/glass-world');
-}
+// async function connectMongoDb(){
+//     // return mongoose.connect('mongodb://127.0.0.1:27017/glass-world');
+// }
 
+let isConnected = false;
+
+async function connectMongoDb() {
+    if (isConnected) return;
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = true;
+    console.log("MongoDB Connected");
+}
 module.exports = {connectMongoDb};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const { connectMongoDb } = require("./connection");
 const routes = require("./routes/user");
+
+dotenv.config();
 
 const app = express();
 
